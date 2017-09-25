27edae7b707aca80d38c63f269f243930d8911a1
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-09-25 20:51:44 +0100

    Corregido codigo

--- espectro_respuesta.m
+++ espectro_respuesta.m
@@ -1,9 +1,12 @@
-function [Sd, Sv, Sa, T, b, TTT] = espectro_respuesta(vg, Fs, plot)
+function [Sd, Sv, Sa, T, b, TTT] = espectro_respuesta(vg, Fs, b, plot, plottitle)
 % Crea el espectro de respuesta a partir de un registro de aceleraciones.
 %
 % Input:
-%   Vg: Señal terremoto (vector de aceleraciones)
-%   Fs: Número de muestras por segundo
+%   Vg:         Señal terremoto (vector de aceleraciones)
+%   Fs:         Número de muestras por segundo
+%   b:          Razón de amortiguamiento
+%   plot:       Indica si grafica o no (booleano)
+%   plottitle:  Título del plot
 %
 % Output:
 %   Sd: Vector desplazamiento
@@ -12,7 +15,22 @@ function [Sd, Sv, Sa, T, b, TTT] = espectro_respuesta(vg, Fs, plot)
 %   T: Vector período
 %   b: Amortiguamiento
 
-if ~exist('plot', 'var'), plot = true; end
+function [t] = max_t(columna, Fs)
+% Retorna el tiempo asociado a la máxima aceleración calculada por el
+% oscilador para un tiempo y una razón de amortiguamiento.
+
+m = size(columna);
+a = 0;
+for i = 1:m
+    % Almacena la máxima aceleración y guarda el tiempo asociado
+    if abs(a) < abs(columna(i))
+        a = abs(columna(i));
+        t = i / Fs;
+    end
+end
+
+%% Si no se define plot se deja falso
+if ~exist('plot', 'var'), plot = false; end
 
 %% Se aplica corrección de linea base a la señal
 vg = detrend(vg);
@@ -20,8 +38,6 @@ vg = detrend(vg);
 %% Crea arreglos de tiempo y razón de amortiguamiento
 dper = 0.01; % Delta de periodo
 T = 0.01:dper:10; % Vector de periodo
-% b = [0 2 5 10 20] / 100; % Razón de amortiguamiento
-b = 5 / 100; % Amortiguamiento del 5%
 
 %% Dimensiones
 nT = length(T);
@@ -31,37 +47,45 @@ nb = length(b);
 Sd = zeros(nT, nb); % Vector de ceros para iniciar vectores de desplazamiento
 Sa = zeros(nT, nb); % Vector de ceros para iniciar vectores de aceleración
 Sv = zeros(nT, nb); % Vector de ceros para iniciar vectores de velocidad
-
-%%
 TTT = zeros(nT, nb); % Tiempos de máxima aceleración para cada combinación amortiguamiento-tiempo
 
-% Se recorre cada combinación de amortiguamiento-periodo
+%% Se recorre cada combinación de amortiguamiento-periodo
 for j = 1:nb
     for i = 1:nT
-        [xm, vm, am] = respcacr(1, T(i), b(j), -vg, Fs, 0, 0); % Se obtiene desplazamiento, velocidad y aceleración
-        Sd(i, j) = max(abs(xm)); % Se guarda el máximo desplazamiento
-        Sv(i, j) = max(abs(vm)); % Se guarda la máxima velocidad
-        Sa(i, j) = max(abs(am+vg)); % Se guarda la máxima aceleración, suma movimiento de la base
-        TTT(i, j) = max_t(am, Fs); % Tiempo asociado a la máxima aceleración
+        % Se obtiene desplazamiento, velocidad y aceleración
+        [xm, vm, am] = respcacr(1, T(i), b(j), -vg, Fs, 0, 0);
+        
+        % Se guarda el máximo desplazamiento
+        Sd(i, j) = max(abs(xm));
+        
+        % Se guarda la máxima velocidad
+        Sv(i, j) = max(abs(vm));
+        
+        % Se guarda la máxima aceleración, suma movimiento de la base
+        Sa(i, j) = max(abs(am+vg));
+        
+        % Tiempo asociado a la máxima aceleración
+        TTT(i, j) = max_t(am, Fs);
     end
 end
 
 %% Grafica los resultados
 if plot
+    figure('name', plottitle);
     subplot(3, 1, 1);
     semilogx(T, Sd, 'k');
     grid on;
-    title('Response spectrum');
-    ylabel('Sd (cm)');
-    xlabel('Period (s)');
+    title(plottitle);
+    ylabel('$S_D (cm)$', 'Interpreter', 'latex');
+    xlabel('Periodo $(s)$', 'Interpreter', 'latex');
     subplot(3, 1, 2);
     semilogx(T, Sv, 'k');
     grid on;
-    ylabel('Sv (cm/s)');
-    xlabel('Period (s)');
+    ylabel('$S_V (cm/s)$', 'Interpreter', 'latex');
+    xlabel('Periodo $(s)$', 'Interpreter', 'latex');
     subplot(3, 1, 3);
-    semilogx(T, Sa./980, 'k'); % Se convierte de cm/s2 a g (m/s2)
+    semilogx(T, Sa./980, 'k');
     grid on;
-    ylabel('Sa (g)');
-    xlabel('Period (s)');
+    ylabel('$S_A (g)$', 'Interpreter', 'latex');
+    xlabel('Periodo $(s)$', 'Interpreter', 'latex');
 end
